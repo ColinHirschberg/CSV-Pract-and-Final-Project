c0ddf886fb18bb4f5c9d1b1addd7a345680ae15c
ColinHirschberg <97074574+ColinHirschberg@users.noreply.github.com>
2022-02-27 23:32:57 -0800

    Outline scrapeVerlanTweets.py
    I delineated five sections of any effective scrapeTweets.py that I have committed to memory

---
+++ scrapeVerlanTweets.py
@@ -0,0 +1,15 @@
+
+#Section 1: In order to implement or wield the scrapeVerlanTweets.py to garner Tweets from Twitter, install pandas and the snscrape packages in the Repl.it 
+#bash shell, not in the python interpreter. The shell defaults to bash mode until "python" is typed in a commandline.
+pip install pandas
+pip3 install git+https://github.com/JustAnotherArchivist/snscrape.git
+
+#Section 2: Iterate through a list of verlan words, and for each verlan word detected, append the tweet featuring the verlan word to a new list.'
+
+#Section 3: Construct a dataframe from the list of verlan words. The dataframe should be organized into a column carrying number tags indicative
+#of metadata of the Tweets and a column carrying the content of the Tweets.
+
+#Section 4: Apply the .head() method or .tail() method to display the first five entries of the dataframe or the final five entries of the dataframe, respectively.
+
+#Section 5: Export or transfer the newly scraped data to a csv.
+
